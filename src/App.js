9f5f3d94c8e2f67b115b6828f1a6b4c65017fc1a
Milan Rathod <mrmilan9.8@gmail.com>
2021-09-29 06:15:27 +0530

    Implemented Accordion UI

--- src/App.js
+++ src/App.js
@@ -1,6 +1,26 @@
 import React from "react";
 import Accordion from "./Accordion";
 
+const items = [
+  {
+    title: "What is React?",
+    content: "React is a front end javascript framework",
+  },
+  {
+    title: "Why use React?",
+    content: "React is favorite JS library among engineers",
+  },
+  {
+    title: "How do you use React?",
+    content: "You use react by creating components",
+  },
+];
+
 export default () => {
-  return (<div> <Accordion/> </div>);
+  return (
+    <div>
+      <br />
+      <Accordion items={items} />{" "}
+    </div>
+  );
 };
