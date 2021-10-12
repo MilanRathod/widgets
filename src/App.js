bd32825ec30d6f09f163a5d80147097f2e5332e1
Milan Rathod <mrmilan9.8@gmail.com>
2021-10-12 06:14:55 +0530

    Added Header Component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Search from "./components/Search";
 import Dropdown from "./components/Dropdown";
 import Translate from "./components/Translate";
 import Route from "./Route";
+import Header from "./components/Header";
 
 const items = [
   {
@@ -41,35 +42,12 @@ const showComponent = (route,component)=>{
   :null;
 }
 
-const showAccordion = ()=>{
-  if(window.location.pathname === '/'){
-    return <Accordion items={items} />;
-  }
-}
-
-const showList = ()=>{
-  if(window.location.pathname === '/list'){
-    return <Search/>;
-  }
-}
-
-const showDropdown = ()=>{
-  if(window.location.pathname === '/dropdown'){
-    return <Dropdown/>;
-  }
-}
-
-const showTranslate = ()=>{
-  if(window.location.pathname === '/translate'){
-    return <Translate/>
-  }
-}
-
 export default () => {
   const [selected, setSelected] = useState(options[0]);
   
   return (
     <div>
+      <Header />
       <Route path="/">
         <Accordion items={items} />
       </Route>
