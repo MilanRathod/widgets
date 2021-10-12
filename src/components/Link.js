150417407ef19877ccd37641793b350cbe9da30f
Milan Rathod <mrmilan9.8@gmail.com>
2021-10-12 06:54:47 +0530

    Handling CTRL/Command Clicks

--- src/components/Link.js
+++ src/components/Link.js
@@ -5,6 +5,10 @@ import React from "react";
 const Link = ({ className, href, children }) => {
 
     const onClick = (event)=>{
+        if(event.metaKey || event.ctrlKey){
+            return;
+        }
+
         event.preventDefault();
         window.history.pushState({},'',href);
     
